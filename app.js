dc7c0c760e7057298d34beb86e192ea1fddd6f6f
kml@kmldev.com <kamillo57@wp.pl>
2020-10-07 17:31:53 +0100

    Add score count function,update result function
    Score function has been add and score are counting and display
    checkResult function has been updated. A few new condition
    has been add. when is no more moves to do game is over.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let gameBoard= document.querySelector('.game');
 document.addEventListener('keyup', control)
+let scoreValue=document.getElementById("gameScore");
+document.getElementById("newgame").addEventListener("click",newGame);
+let score=0; 
 let game =[
     [0,0,0,0],
     [0,0,0,0],
@@ -7,14 +10,21 @@ let game =[
     [0,0,0,0]
 ] 
 
+function newGame()
+{
+    location.reload(); 
+    
+}
+
 function generate()
 {
     for(let i=0;i<4;i++)
     {
         for(let j=0;j<4;j++)
         {
-            let element =document.createElement('div')
-            element.innerHTML= game[i][j];
+            let element =document.createElement('div');
+            (game[i][j]==0)?element.innerHTML="":element.innerHTML= game[i][j];
+            
             element.id=String(i)+String(j);
             gameBoard.appendChild(element);  
         }
@@ -31,9 +41,16 @@ function update()
         {   
            id = String(i)+String(j);
            element = document.getElementById(id);
-           element.innerHTML=game[i][j];  
+           (game[i][j]==0)?element.innerHTML="":element.innerHTML= game[i][j]; 
         }
     }
+    
+
+}
+
+function showScore()
+{
+    scoreValue.innerHTML = score;
 }
 
 function randomElement()
@@ -48,146 +65,294 @@ function addNewElement()
 
     (game[x][y]==0)? game[x][y]=2: addNewElement();   
 }
-// split swiping a blocks and adding blocks !!!!! IMPORTANT
-function moveRight()
+
+
+
+function swipe(direction)
 {
+
+    for(let i=0;i<4;i++)
+            {
+               do {
+                    mark=false;
+
+                    if(direction=="right" || direction=="down")
+                    {
+                        for(let j=0;j<game.length-1;j++) 
+                        {
+                            if(j<game.length-1)// if array is not ouf bound 
+                            {
+                                switch(direction)
+                                {
+                                    case "right":
+                                        if(game[i][j]>0 && game[i][j+1]==0)
+                                        {
+                                            let tmp=game[i][j];
+                                            game[i][j]=game[i][j+1];
+                                            game[i][j+1]=tmp; 
+                                            mark=true;
+                                        }  
+                                        break;
+                                    case "down":
+                                        if(game[j][i]>0 && game[j+1][i]==0)
+                                        {
+                                            let tmp=game[j][i];
+                                            game[j][i]=game[j+1][i];
+                                            game[j+1][i]=tmp; 
+                                            mark=true;
+                                        } 
+                                        break;
+                                }
+                               
+                            }
+                        } 
+                    }
+
+                    if(direction=="left" || direction=="up")
+                    {
+                        for(let j=game.length-1;j>=0;j--)
+                        {
+                            if(j>0)// if array is not ouf bound 
+                                {
+                                    switch(direction)
+                                    {
+                                        case "left":
+                                            if(game[i][j]>0 && game[i][j-1]==0)
+                                            {
+                                                let tmp=game[i][j];
+                                                game[i][j]=game[i][j-1];
+                                                game[i][j-1]=tmp; 
+                                                mark=true;
+                                            }           
+                                            break;
+                                        case "up":
+                                            if(game[j][i]>0 && game[j-1][i]==0)
+                                            {
+                                                let tmp=game[j][i];
+                                                game[j][i]=game[j-1][i];
+                                                game[j-1][i]=tmp; 
+                                                mark=true;
+                                            }        
+                                            break;
+                                    }
+                                             
+                                }
+                        }
+                    }
     
-    move("right");
-    
-    addNewElement();
+               }while(mark==true)
+    }
+
 }
 
-function moveDown()
+function sumElemnets(direction)
 {
-    move("down");
-    addNewElement();
-}
-
-function moveLeft()
-{
-   move("left");
-    
-    addNewElement();
-}
-
-function moveUp()
-{
-    move("up");
-    addNewElement();
-}
-
-function move(direction)
-{
-    let col0=[];
-    let col1=[];
-    let col2=[];
-    let col3=[];
-    let columns=[];
-    let arraySize=4;
-    switch(direction)
+    for(let i=0;i<4;i++) 
     {
-        case "right":
-            for(let i=0;i<4;i++)
+        if(direction=="left" || direction=="up")
+        {
+            for(let j=0;j<game.length-1;j++) 
             {
-                let tmp=game[i].filter(item=>item>0);
-                let zeroArr = Array(arraySize-tmp.length).fill(0);
-                game[i] = zeroArr.concat(tmp);
-                console.log(game[i]);  
-                   
-            }
-            
-            break;
-        case "left":
-            for(let i=0;i<4;i++)
-            {
-                let tmp=game[i].filter(item=>item>0);
-                let zeroArr = Array(arraySize-tmp.length).fill(0);
-                game[i] = tmp.concat(zeroArr);
-                console.log(game[i]);  
-                   
-            }
-            break;
+                if(j<game.length-1)// if array is not ouf bound 
+                {
+                    switch(direction)
+                    {
+                        case "left":
+                            if(game[i][j] == game[i][j+1])
+                            {
+                                
+                                game[i][j]=game[i][j]+game[i][j+1];
+                                game[i][j+1]=0; 
+                                score+=game[i][j];
+                                
+                            }  
+                            break;
+                        case "up":
+                            if(game[j][i] == game[j+1][i])
+                            {
+                                
+                                game[j][i]=game[j][i]+game[j+1][i];
+                                game[j+1][i]=0; 
+                                score+=game[j][i];
+                                
+                            }  
+                            break;
+                    }
+                    
+                } 
 
-        case "down":
-            
-            for(let i=0;i<4;i++)
-            {
-                col0.push(game[i][0]);
-                col1.push(game[i][1]);
-                col2.push(game[i][2]);
-                col3.push(game[i][3]);
             }
-            columns=[col0,col1,col2,col3];
-            for(let i=0;i<4;i++)
-            {
-                let tmp=columns[i].filter(item=>item>0);
-                let zeroArr = Array(arraySize-tmp.length).fill(0);
-                columns[i] = tmp.concat(zeroArr);
-                console.log(columns[i]);  
-                   
-            }
-            break;
-        case "up":
-            
-                for(let i=3;i>=0;i--)
-                {
-                    console.log(game[i][0]);
-                    col0.push(game[i][0]);
-                    col1.push(game[i][1]);
-                    col2.push(game[i][2]);
-                    col3.push(game[i][3]);
-                }
-                columns=[col0,col1,col2,col3];
-                
-                for(let i=0;i<4;i++)
-                {
-                    let tmp=columns[i].filter(item=>item>0);
-                    let zeroArr = Array(arraySize-tmp.length).fill(0);
-                    columns[i] = zeroArr.concat(tmp);
-                    console.log(columns[i]);  
-                       
-                }
-                break;
+        }
+
+        if(direction=="right" || direction=="down")
+                    {
+                        for(let j=game.length-1;j>=0;j--)
+                        {
+                            if(j>0)// if array is not ouf bound 
+                                {
+                                    switch(direction)
+                                    {
+                                        case "right":
+                                            if(game[i][j] == game[i][j-1])
+                                            {
+                                                game[i][j]=game[i][j] + game[i][j-1];
+                                                game[i][j-1]=0;
+                                                score+=game[i][j];
+                                                
+                                            }           
+                                            break;
+                                        case "down":
+                                            if(game[j][i] == game[j-1][i])
+                                            {
+                                                game[j][i]=game[j][i] + game[j-1][i];
+                                                game[j-1][i]=0;
+                                                score+=game[j][i];
+                                                
+                                            }        
+                                            break;
+                                    }
+                                             
+                                }
+                        }
+                    }
+
         
     }
-    
 }
 
+function checkRestult()
+{
+    let moves=0;
+    for(let i=0;i<game.length;i++) 
+    {
+        for(let j=0;j<game.length;j++) 
+        {
+           
+            if(game[i][j]==0)
+            {
+                moves++;
+                
+            }
+        }
+        
+    }
+    if(moves==0)
+    {
+        for(let i=0;i<game.length;i++) 
+        {
+            for(let j=0;j<game.length;j++) 
+            {
+               
+                //to be continued 
+                if(i==0 && j==0)
+                {
+                    (game[i][j]==game[i][j+1])? moves++:moves=moves;
+                    (game[i][j]==game[i+1][j])? moves++:moves=moves;
+                }
+                if(i==0 && j==3)
+                {
+                    (game[i][j]==game[i][j-1])? moves++:moves=moves;
+                    (game[i][j]==game[i+1][j])? moves++:moves=moves;
+                }
+                if(i==3 && j==0)
+                {
+                    (game[i][j]==game[i][j+1])? moves++:moves=moves;
+                    (game[i][j]==game[i-1][j])? moves++:moves=moves;
+                }
+                if(i==3 && j==3)
+                {
+                    (game[i][j]==game[i][j-1])? moves++:moves=moves;
+                    (game[i][j]==game[i-1][j])? moves++:moves=moves;
+                }
+                if(i==1 && j==1 || i==1 && j==2 || i==2 && j==2 || i==2 && j==3)
+                {
+                    (game[i][j]==game[i+1][j])? moves++:moves=moves;
+                    (game[i][j]==game[i-1][j])? moves++:moves=moves;
+                    (game[i][j]==game[i][j+1])? moves++:moves=moves;
+                    (game[i][j]==game[i][j-1])? moves++:moves=moves;
 
+                } 
+                if(i==1 && j==0 || i==2 && j==0)
+                {
+                    (game[i][j]==game[i+1][j])? moves++:moves=moves;
+                    (game[i][j]==game[i-1][j])? moves++:moves=moves;
+                    (game[i][j]==game[i][j+1])? moves++:moves=moves;
+                }
 
+                if(i==1 && j==3 || i==2 && j==3)
+                {
+                    (game[i][j]==game[i+1][j])? moves++:moves=moves;
+                    (game[i][j]==game[i-1][j])? moves++:moves=moves;
+                    (game[i][j]==game[i][j-1])? moves++:moves=moves;
+                }
+
+                if(i==0 && j==1 || i==0 && j==2)
+                {
+                    (game[i][j]==game[i][j+1])? moves++:moves=moves;
+                    (game[i][j]==game[i][j]-1)? moves++:moves=moves;
+                    (game[i][j]==game[i+1][j])? moves++:moves=moves;
+                }
+
+                if(i==3 && j==1 || i==3 && j==2)
+                {
+                    (game[i][j]==game[i][j+1])? moves++:moves=moves;
+                    (game[i][j]==game[i][j]-1)? moves++:moves=moves;
+                    (game[i][j]==game[i-1][j])? moves++:moves=moves;
+                }
+
+            }
+            
+        }
+    }
+   
+    
+    if(moves==0)
+    {
+        alert("Game over");
+    }
+
+}
 function control(e) {
     if(e.keyCode === 37) {
-      keyLeft()
-    } else if (e.keyCode === 38) {
-      keyUp()
-    } else if (e.keyCode === 39) {
-      keyRight()
-    } else if (e.keyCode === 40) {
-      keyDown()
+        swipe("left");
+        sumElemnets("left");
+        swipe("left");
+        update();
+        showScore();
+        addNewElement();
+        update();
+        checkRestult();
+    } 
+    if (e.keyCode === 38) {
+        swipe("up");
+        sumElemnets("up");
+        swipe("up");
+        update();
+        showScore();
+        addNewElement();
+        update();
+        checkRestult();
+    } 
+    if (e.keyCode === 39) {
+        swipe("right");
+        sumElemnets("right");
+        swipe("right");
+        update();
+        showScore();
+        addNewElement();
+        update();
+        checkRestult();
+    } 
+    if (e.keyCode === 40) {
+        swipe("down");
+        sumElemnets("down");
+        swipe("down");
+        update();
+        showScore();
+        addNewElement();
+        update();
+        checkRestult();
     }
-  }
-  
-  function keyRight() {
-    moveRight()
-    update()
-    
-    
-  }
-
-  function keyLeft() {
-    moveLeft();
-    update();
-    
-  }
-
-  function keyUp() {
-    moveUp()
-    update();
-  }
-
-  function keyDown() {
-    moveDown()
-    update();
   }
 
 function init()
@@ -198,3 +363,4 @@ function init()
 }
 
 init();
+
